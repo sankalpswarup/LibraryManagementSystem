b82caa2227a8e3aa19eb04b02b9485f4f12303ae
Sankalp Swarup <sankalpswaroop@gmail.com>
2023-11-28 01:32:04 +0530

    Changes made for updated version of Django

--- LibManage/home/urls.py
+++ LibManage/home/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path, re_path
+# from django.conf.urls import url
+# from django.conf.urls import re_path
 from home import views
 
 urlpatterns = [
